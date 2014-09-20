56d3548658c737e827f6cb3db8db06600429b0c3
Miranda Antenesse <miranda@antenesse.net>
2014-09-20 14:35:55 -0700

    adds functionality to determine area of rectrangle

--- area.py
+++ area.py
@@ -2,6 +2,7 @@
 if __name__ == '__main__':
         print 'Choose the shape for which you want to determine area:'
         print '(1) Square'
+        print '(2) rectangle'
 
         shape_selection = raw_input()
 
@@ -10,5 +11,15 @@ if __name__ == '__main__':
                 length = raw_input()
                 area = int(length) * int(length)
                 print 'The area is %i' % (area)
+        elif shape_selection =='2':
+                print 'Please enter the length of one side:'
+                length = raw_input()
+                print 'Please enter the width of one side:'
+                width = raw_input()
+                area = int(length) * int(width)
+                print 'The area is %i' % (area)
         else:
                 print 'NOT A VALID SELCTION'
+
+
+
